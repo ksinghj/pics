7d093a808a713bc486ec43597464e89877cdd0aa
Kartar Jabanda <kartar.jabanda@remarkable.net>
2020-02-14 16:27:25 +0000

    finish section 10

--- src/Components/ImageList.jsx
+++ src/Components/ImageList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import '../styles/ImageList.css';
+import ImageCard from './ImageCard'
 
 const ImageList = props => {
-    const images = props.images.map(({urls, description, id}) => {
-        return <img src={urls.regular} alt={description} key={id} />
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
